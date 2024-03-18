e5c74ff7c6e2de23d55fe57a1e94eedbe42055c2
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-18 23:49:26 +0000

    Basic async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This the first module 0x01-python_async_function project"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """This a couroutine function
+    """
+    random_value = random.uniform(0, max_delay)
+    await asyncio.sleep(random_value)
+    return random_value
