892aaf954ba740f6702bf74e29f44985e8a1ed70
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-18 23:55:57 +0000

    add annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int=10)->float:
+async def wait_random(max_delay: int = 10) -> float:
     """This a couroutine function
     """
     random_value = random.uniform(0, max_delay)
