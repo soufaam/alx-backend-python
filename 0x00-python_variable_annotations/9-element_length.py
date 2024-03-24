77934e61ffa4546bb662f6558c8f3150bac08fa8
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-24 23:59:55 +0000

    9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""ADD float module
+"""
+from typing import List, Typle, Any, Sequence
+
+
+def element_length(lst: Sequence) -> List[Tuple[typing.Sequence, int]]:
+    return [(i, len(i)) for i in lst]
