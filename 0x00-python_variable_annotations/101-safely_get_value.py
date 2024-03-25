fae1f9d4400f632af7175efac601b51938f452fa
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-25 00:29:25 +0000

    klajfda

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""ADD float module
+"""
+from typing import List, Any, Union, Mapping, TypeVar
+
+
+T = TypeVar("T")
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, type(None)] =
+                     None) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
